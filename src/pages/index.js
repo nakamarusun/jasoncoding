d9f442d39c944d7bc42282101e97961d6e484324
Jason Christian H <nakamarujc@gmail.com>
2022-02-06 20:35:51 +0700

    Added OG tags with splash image

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Header from "../components/Header/Header";
 import { Helmet } from "react-helmet";
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 import Sidebar from "../components/Sidebar";
+import splash from "../images/splash.webp";
 
 const IndexPage = () => {
   return (
@@ -18,6 +19,31 @@ const IndexPage = () => {
           name="description"
           content="Jasoncoding. Jason Christian Hailianto programming and development portfolio website"
         />
+        {/* Og graphs */}
+        <meta property="og:url" content="https://jasoncoding.com/" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="Jasoncoding - 🇮🇩 Full Stack Engineer + Designer"
+        />
+        <meta
+          property="og:description"
+          content="Hello! My name is Jason Christian Hailianto. This is my portfolio and blogging website about my projects and whatever else that i want to post."
+        />
+        <meta property="og:image" content={splash} />
+        {/* Twitter graphs */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="jasoncoding.com" />
+        <meta property="twitter:url" content="https://jasoncoding.com/" />
+        <meta
+          name="twitter:title"
+          content="Jasoncoding - 🇮🇩 Full Stack Engineer + Designer"
+        />
+        <meta
+          name="twitter:description"
+          content="Hello! My name is Jason Christian Hailianto. This is my portfolio and blogging website about my projects and whatever else that i want to post."
+        />
+        <meta name="twitter:image" content={splash} />
       </Helmet>
       <Sidebar />
       <Header />
